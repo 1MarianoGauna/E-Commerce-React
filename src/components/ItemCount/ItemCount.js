9fbe89c1adcb278f5d1696a8f098ea143bb1a1ee
Mariano Gauna <mar_iao@hotmail.com>
2021-09-21 00:49:14 -0300

    arregle el contador

--- src/components/ItemCount/ItemCount.js
+++ src/components/ItemCount/ItemCount.js
@@ -1,19 +1,26 @@
 import * as React from 'react';
+import react from 'react';
 import './ItemCount.css'
 
 function ItemCount({stock, initial}){
-    const [count, setCount] = React.useState(1);
+    const [count, setCount] = React.useState(initial);
     const sumar =  () => {
-        setCount ((prevResult) => prevResult + 1)
+        setCount ((prevState) => prevState + 1)
     }
     const restar = () => {
-        setCount ((prevResult) => prevResult - 1)
+        setCount ((prevState) => prevState - 1)
+    }
+    if (count > 5){
+        setCount ((prevState) => prevState = 5)
+    }
+    else if(count < 1){
+        setCount ((prevState) => prevState = 1)
     }
     return (
         <div className='itemCount'>
-            <button onClick={restar} className='itemCount__suma'>-</button>
+            <button onClick={restar} id='myButtonSumar' className='itemCount__suma'>-</button>
             <p>{count}</p>
-            <button onClick={sumar} className='itemCount__resta'>+</button>
+            <button onClick={sumar} id='myButtonResta' className='itemCount__resta'>+</button>
         </div>
     )
 }
--- src/components/ItemList/ItemListContainer.js
+++ src/components/ItemList/ItemListContainer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from '../Card/Card';
+import ItemCount from '../ItemCount/ItemCount';
 
 function ItemListContainer (){
     const comprar = (productId) =>{
@@ -7,13 +8,13 @@ function ItemListContainer (){
     }
     return (
         <section className='myCards'>
-        <Card productId='1' title='Esto es un producto 1' src='./images/imagen1.jpg' description='Esta es la descripción 1' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='2' title='Esto es un producto 2' src='./images/imagen1.jpg' description='Esta es la descripción 2' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='3' title='Esto es un producto 3' src='./images/imagen1.jpg' description='Esta es la descripción 3' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='4' title='Esto es un producto 4' src='./images/imagen1.jpg' description='Esta es la descripción 4' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='5' title='Esto es un producto 5' src='./images/imagen1.jpg' description='Esta es la descripción 5' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='6' title='Esto es un producto 6' src='./images/imagen1.jpg' description='Esta es la descripción 6' comprando={comprar} stock='5' initial='1'/>
-        <Card productId='7' title='Esto es un producto 7' src='./images/imagen1.jpg' description='Esta es la descripción 7' comprando={comprar} stock='5' initial='1'/>
+        <Card productId='1' title='Esto es un producto 1' src='./images/imagen1.jpg' description='Esta es la descripción 1' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='2' title='Esto es un producto 2' src='./images/imagen1.jpg' description='Esta es la descripción 2' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='3' title='Esto es un producto 3' src='./images/imagen1.jpg' description='Esta es la descripción 3' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='4' title='Esto es un producto 4' src='./images/imagen1.jpg' description='Esta es la descripción 4' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='5' title='Esto es un producto 5' src='./images/imagen1.jpg' description='Esta es la descripción 5' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='6' title='Esto es un producto 6' src='./images/imagen1.jpg' description='Esta es la descripción 6' comprando={comprar} stock={5} initial={1}/>
+        <Card productId='7' title='Esto es un producto 7' src='./images/imagen1.jpg' description='Esta es la descripción 7' comprando={comprar} stock={5} initial={1}/>
         </section>
     )
 }
