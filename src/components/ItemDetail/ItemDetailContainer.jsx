d29d48c787acbccbd413c5ecd971e4224969d8cc
Mariano Gauna <mar_iao@hotmail.com>
2021-10-14 17:28:14 -0300

    arregle mas la firestore

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -1,109 +1,38 @@
 import * as React from 'react';
 import { useParams } from 'react-router';
+import { getFireStore } from '../../firebase';
 import Card from '../Card/Card';
 
-const ItemDetailContainer = () => {
+const ItemDetailContainer = ({producto}) => {
     const [prod, setProd] = React.useState([]);
     const { id } = useParams();
 
+
     React.useEffect(() => {
-        getProducts().then((result) => setProd(result))
-    }, [])
-    const getProducts = () => {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(filtro)
-            }, 2000)
-        })
-    }
-    const filtro = productos.filter(prod => prod.id == id)
+        const database = getFireStore();
+        const producCol = database.collection('productos');
+        const myProduct = producCol.doc(id);
+        
+        myProduct.get().then((doc) => {
+            if(!doc.exists){
+                console.log('No existe')
+            }else{
+                setProd({id: doc.id, ...doc.data()})
+            }
+        }).catch(() => {})
+        .finally(() => {})
+    }, [id])
     return (
-        <section className='myCards'>
-            {prod?.map((producto) => {
-                return (
-                    <div key={producto.id}>
-                        <Card
-                            key={producto.id}
-                            title={producto.title}
-                            productId={producto.id}
-                            src={producto.image}
-                            description={producto.description}
-                            precio={producto.precio}
-                            stock={producto.stock}
-                            initial={producto.initial}
-                            producto={producto}
-                        />
-                    </div>
-                );
-            })}
-        </section>
+        <Card
+        title={prod?.title}
+        src={prod?.image}
+        description={prod?.description}
+        precio={prod?.precio}
+        stock={prod?.stock}
+        initial={prod?.initial}
+        producto={producto}
+        />
     )
 }
 
 export default ItemDetailContainer
-
-const productos = [
-    {
-        id: 0,
-        title: 'Esto es un producto 0',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 0',
-        precio: 5000,
-        stock: 9,
-        initial: 1
-    },
-    {
-        id: 1,
-        title: 'Esto es un producto 1',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 1',
-        precio: 5000,
-        stock: 8,
-        initial: 1
-    },
-    {
-        id: 2,
-        title: 'Esto es un producto 2',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 2',
-        precio: 5000,
-        stock: 2,
-        initial: 1
-    },
-    {
-        id: 3,
-        title: 'Esto es un producto 3',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 3',
-        precio: 5000,
-        stock: 5,
-        initial: 1
-    },
-    {
-        id: 4,
-        title: 'Esto es un producto 4',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 4',
-        precio: 5000,
-        stock: 3,
-        initial: 1
-    },
-    {
-        id: 5,
-        title: 'Esto es un producto 5',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 5',
-        precio: 5000,
-        stock: 4,
-        initial: 1
-    },
-    {
-        id: 6,
-        title: 'Esto es un producto 6',
-        image: '/images/imagen1.jpg',
-        description: 'Esta es la descripcion 6',
-        precio: 5000,
-        stock: 6,
-        initial: 1
-    },
-]
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -4,6 +4,7 @@ import Card from '../Card/Card';
 
 const ItemList = () => {
     const [prod, setProd] = React.useState([]);
+    const [error, setError] = React.useState(null)
     React.useEffect(() => {
         const db = getFireStore();
         const producCol = db.collection('productos');
@@ -14,20 +15,9 @@ const ItemList = () => {
             }else{
                 setProd(querySnapshot.docs.map((doc) => ({id: doc.id, ...doc.data()})))
             }})
-            .catch((querySnapshot) => console.log('no funca'))
+            .catch((error) => setError(error))
 
     }, []);
-    /*React.useEffect(() => {
-        getProducts().then((result) => setProd(result))
-    }, [])*/
-    /*     const getProducts = () => {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve(productos)
-                }, 2000)
-            })
-        } */
-
     return (
         <section className='myCards'>
             {prod?.map((producto) => {
@@ -50,68 +40,3 @@ const ItemList = () => {
     )
 }
 export default ItemList
-/* const productos = [
-    {
-        id: 0,
-        title: 'Esto es un producto 0',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 0',
-        precio: 5000,
-        stock: 9,
-        initial: 1
-    },
-    {
-        id: 1,
-        title: 'Esto es un producto 1',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 1',
-        precio: 5000,
-        stock: 8,
-        initial: 1
-    },
-    {
-        id: 2,
-        title: 'Esto es un producto 2',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 2',
-        precio: 5000,
-        stock: 2,
-        initial: 1
-    },
-    {
-        id: 3,
-        title: 'Esto es un producto 3',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 3',
-        precio: 5000,
-        stock: 5,
-        initial: 1
-    },
-    {
-        id: 4,
-        title: 'Esto es un producto 4',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 4',
-        precio: 5000,
-        stock: 3,
-        initial: 1
-    },
-    {
-        id: 5,
-        title: 'Esto es un producto 5',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 5',
-        precio: 5000,
-        stock: 4,
-        initial: 1
-    },
-    {
-        id: 6,
-        title: 'Esto es un producto 6',
-        image: './images/imagen1.jpg',
-        description: 'Esta es la descripcion 6',
-        precio: 5000,
-        stock: 6,
-        initial: 1
-    },
-] */
